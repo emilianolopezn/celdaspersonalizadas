712259ac87b7bef38c72df6223490c16ee3fb80b
Alumno <ulsa@iMacD1Maestro.local>
2018-09-18 18:50:38 -0700

    Celdas

---
+++ celdaspersonalizadas/CeldaAlumno.swift
@@ -0,0 +1,16 @@
+//
+//  CeldaAlumno.swift
+//  celdaspersonalizadas
+//
+//  Created by Alumno on 18/09/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaAlumno : UITableViewCell {
+    
+    @IBOutlet weak var lblPromedio: UILabel!
+    @IBOutlet weak var lblAlumno: UILabel!
+}
--- celdaspersonalizadas/ViewController.swift
+++ celdaspersonalizadas/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let alumnos = ["Benjamin", "Pablo", "Sofia", "Rafa", "Fernanda"]
+    let promedios = [9.9, 9.8, 9.7, 9.6, 9.5]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alumnos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celdaAlumno = tableView.dequeueReusableCell(withIdentifier: "cellAlumno") as? CeldaAlumno
+        
+        
+        celdaAlumno?.lblAlumno.text = alumnos[indexPath.row]
+        celdaAlumno?.lblPromedio.text = "\(promedios[indexPath.row])"
+        
+        
+        return celdaAlumno!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
